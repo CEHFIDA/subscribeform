ed4cc74d67add9c8e9c16cf8b91122a0e3714fe0
Vladislav <laponov@gmail.com>
2017-10-06 14:45:37 +0300

    Update Controller

--- src/SubscribeFormController.php
+++ src/SubscribeFormController.php
@@ -4,7 +4,7 @@ namespace Selfreliance\subscribeform;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Selfreliance\Subscribeform\Models\Contact;
+use Selfreliance\Subscribeform\Models\Subscribe;
 
 class SubscribeFormController extends Controller
 {
@@ -17,9 +17,9 @@ class SubscribeFormController extends Controller
         $check_mail = \DB::table('subscribes')->where('email', $request->input('email'))->first();
         if(!$check_mail)
         {
-	        $model->email = $request->input('email');
-	        $model->save();
-            echo '<div class="alert alert-success">Вы успешно подписались на рассылку!</div>';
+		$model->email = $request->input('email');
+		$model->save();
+            	echo '<div class="alert alert-success">Вы успешно подписались на рассылку!</div>';
         }
         else echo '<div class="alert alert-danger">Данный email уже подписан на рассылку!</div>';
     }
