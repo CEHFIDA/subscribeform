00203cf6758ecda05ebc124271e7b293a1870683
Vladislav <laponov@gmail.com>
2017-10-06 19:42:35 +0300

    Update Subscribe.php

--- src/Models/Subscribe.php
+++ src/Models/Subscribe.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Selfreliance\contactform\Models;
+namespace Selfreliance\subscribeform\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
